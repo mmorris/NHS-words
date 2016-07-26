cc4330cbb2f512ae9aad1cd42448440575ec820f
Matt Morris <me@matt.ph>
2016-07-25 21:59:58 -0400

    Added additional phrases.

--- Source/content.js
+++ Source/content.js
@@ -9,7 +9,11 @@ for (var i = 0; i < elements.length; i++) {
         if (node.nodeType === 3) {
             var text = node.nodeValue;
             var replacedText = text.replace(/NHS Overspend/gi, 'chronic underfunding');
+            var replacedText = replacedText.replace(/overspend by hospitals/gi, 'underfunding of hospitals');
             var replacedText = replacedText.replace(/overspend/gi, 'underfunding');
+            var replacedText = replacedText.replace(/overspent/gi, 'were underfunded');
+            var replacedText = replacedText.replace(/busting their budgets/gi, 'not receiving necessary funding');
+            var replacedText = replacedText.replace(/exceeded their budgets/gi, 'were underfunded');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
